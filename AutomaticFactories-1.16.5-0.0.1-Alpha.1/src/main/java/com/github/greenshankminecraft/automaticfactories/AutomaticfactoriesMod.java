ff6ac1237e5382fd602a5bd29fe9caa9f19f009c
GREENSHANK <bakuhatu3masukato@gmail.com>
2023-06-20 23:05:17 +0900

    test send

---
+++ AutomaticFactories-1.16.5-0.0.1-Alpha.1/src/main/java/com/github/greenshankminecraft/automaticfactories/AutomaticfactoriesMod.java
@@ -0,0 +1,8 @@
+package com.github.greenshankminecraft.automaticfactories;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(AutomaticfactoriesMod.MOD_ID)
+public class AutomaticfactoriesMod {
+    public static final String MOD_ID = "automaticfactoriesmod";
+}
